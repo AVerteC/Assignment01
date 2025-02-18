97aa80607f0afbbfccc170366eee13de32cb1d21
Alan Chen <alrichen@ucsc.edu>
2025-02-18 13:18:55 -0800

    more progress

--- js/script.js
+++ js/script.js
@@ -6,9 +6,37 @@
 // let myLibraryFunction;
 
 // STEP 2
+// 9; // numeric literal expression
+// 'Giraffe'; // string literal expression
+// true; // Boolean literal expression
+// null; // null literal expression
+
 // STEP 3
+let mathResult = 2 + (10%3) * 2; // complex/variable expression 
+console.log(mathResult);
+
+let shoeSize = 6;
+let shoeFits = (shoeSize > 5) && (shoeSize < 7) && (shoeSize !== null); // complex expression
+console.log(shoeFits); // complex/variable expression
+
 // STEP 4
+// let strFirstName;
+// let strLastName;
+// let strAddress;
+// let strCity;
+// let numZipCode;
+// let numYourAge;
+// let strReferralSource;
+// let boolMayWeContactYou;
+
 // STEP 5
+// using strFirstname, numZipCode, boolMayWeContactYou
+// let strFirstName;
+// strFirstName = "Natalie";
+
+// let strFirstName, numZipCode, boolMayWeContactYou = "Daniel", 95060, false;
+
+
 // STEP 6
 // STEP 7
 // STEP 8
