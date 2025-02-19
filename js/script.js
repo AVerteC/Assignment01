ab6a983be845197cb6e63dd5bb62e147e7b08744
Alan Chen <alrichen@ucsc.edu>
2025-02-18 16:33:50 -0800

    tried all steps

--- js/script.js
+++ js/script.js
@@ -12,12 +12,12 @@
 // null; // null literal expression
 
 // STEP 3
-let mathResult = 2 + (10%3) * 2; // complex/variable expression 
-console.log(mathResult);
+// let mathResult = (2 + (10%3)) * 2; // complex/variable expression 
+// console.log("mathResult = " + mathResult);
 
-let shoeSize = 6;
-let shoeFits = (shoeSize > 5) && (shoeSize < 7) && (shoeSize !== null); // complex expression
-console.log(shoeFits); // complex/variable expression
+// let shoeSize = 6;
+// let shoeFits = (shoeSize > 5) && (shoeSize < 7) && (shoeSize !== null); // complex expression
+// console.log( "shoeFits = "+ shoeFits); // complex/variable expression
 
 // STEP 4
 // let strFirstName;
@@ -30,27 +30,110 @@ console.log(shoeFits); // complex/variable expression
 // let boolMayWeContactYou;
 
 // STEP 5
-// using strFirstname, numZipCode, boolMayWeContactYou
+// using the three variables strFirstname, numZipCode, boolMayWeContactYou
+
 // let strFirstName;
 // strFirstName = "Natalie";
 
-// let strFirstName, numZipCode, boolMayWeContactYou = "Daniel", 95060, false;
+// let strFirstName, numZipCode, boolMayWeContactYou;
+// strFirstName = "Daniel", numZipCode = 95060, boolMayWeContactYou = false;
 
+// let strFirstName = Natalie;
 
 // STEP 6
+    // // create variable
+    // let variable;
+    // // add number to variable
+    // result = result + 5;
+    // // add string to variable
+    // result = result + "dogs";
+    // console.log("result = " + result);
+
+    // // or
+    // // create variable
+    // let variable;
+    // // add number to string and display result in console
+    // console.log(5 + "dogs");
+
 // STEP 7
+    // let a;
+    // a = true + "sausage"; 
+    // console.log("a = " + a);
+
+    // let b;
+    // b = 5 + false
+    // console.log("b = " + b);
+
 // STEP 8
+    // // The given string literal is not valid.
+    // // I would fix the string literal by adding an escape character '\' before the ' character in I'm
+    // let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+    // console.log(someString)
+
 // STEP 9
+    // let nullValue = null;
+    // let undefinedValue;
+
 // STEP 10
+    // console.log(typeof "Nathan");
+    // console.log(typeof 5);
+    // console.log(typeof true);
+    // let object = {
+    //     weekday:  "Monday",
+    //     dayOfMonth: 18
+    // };
+    // console.log(typeof object);
+    // console.log(typeof unassignedVariable);
+
 // STEP 11
+    // alert("Hello " + "Alan Chen" + ", welcome to the JavaScript class!");
+
 // STEP 12
+    // let name = "Alan Chen";
+    // alert("Hello " + name + ", welcome to the JavaScript class!");
+
 // STEP 13
+    // let name = "Alan Chen";
+    // let course = "JavaScript";
+    // alert("Hello " + name + ", welcome to the " + course + " class!");
+
 // STEP 14
+    // let name = "Alan Chen";
+    // let course = "JavaScript";
+    // alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
 // STEP 15
+    // let name = prompt("Enter your name:");
+    // let course = "JavaScript";
+    // alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
 // STEP 16
+    // let name = prompt("Enter your name:"); 
+    // let course = prompt("Enter the class that you are taking:");
+    // alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
 // STEP 17
+    // let x = 10;
+    // let y = 20;
+    // console.log("x + y = " + (x + y));
+
 // STEP 18
+    // let x = 20;
+    // x += 20;
+    // console.log("x = " + x);
+
 // STEP 19
+    // let x = 20;
+    // x *= 5;
+    // console.log("x = " + x);
+
 // STEP 20
-// STEP 21
+    // let x = 20 % 3;
+    // x /= 1;
+    // console.log("x = " + x);
+
+// STEP 21    
+    // console.log((70 - 8) >= 60 || (10 + 8) != 18);
+
 // STEP 22
+    // console.log((10 + 3) < 101 && (10 + 3) === 5); 
